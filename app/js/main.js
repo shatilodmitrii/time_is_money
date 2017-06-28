47fcc50c0515daeae34e8115585ad1c2cdc78ed6
Shatilo Dmitrii <shatilodmitrii@gmail.com>
2017-06-29 01:52:42 +0700

    added team service contact

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
 $("#owl-example").owlCarousel({
-	singleItem: true,
-	navigation: true,
-	navigationText: false
+    singleItem: true,
+    pagination: true,
+    navigation: true,
+    navigationText: false
+  });
+$("#but1").mouseover(function(){$("#top_text1").css("background-color","#44c0c2")});
+$("#but1").mouseout(function(){$("#top_text1").css("background-color","#485460")});
+$("#but2").mouseover(function(){$("#top_text2").css("background-color","#44c0c2")});
+$("#but2").mouseout(function(){$("#top_text2").css("background-color","#485460")});
+$("#but3").mouseover(function(){$("#top_text3").css("background-color","#44c0c2")});
+$("#but3").mouseout(function(){$("#top_text3").css("background-color","#485460")});
 });
